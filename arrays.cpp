f9ca0c1d14bb0828b6c823767c54f3667e56c7f9
DhayanandhS <dhaya.pit180@gmail.com>
2024-05-16 16:41:46 +0530

    second commit

---
+++ arrays.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+int main(){
+    int arr[5];
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    cout<<arr[1];
+}
