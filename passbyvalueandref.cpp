9cbb0128e740818be3a8d3ba36f09afbc83facd3
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-14 20:41:53 +0530

    added Day1.java

--- passbyvalueandref.cpp
+++ passbyvalueandref.cpp
@@ -2,8 +2,18 @@
 #include <string>
 
 using namespace std;
-
+//pass by value - created a copy of num1 and gave it to the addition function
 int addition(int num1){
+    cout << num1 << endl;
+    num1 += 5;
+    cout << num1 << endl;
+    num1 += 5;
+    cout << num1 << endl;
+    return 0;
+} 
+
+// pass by rerfernce - we are passing the address of the value num1
+int addition2(int &num1){
     cout << num1 << endl;
     num1 += 5;
     cout << num1 << endl;
@@ -16,6 +26,8 @@ int main(){
     int num1;
     num1 = 10;
     addition(num1);
+    cout << num1 << endl;
+    addition2(num1);
     cout << num1;
     return 0;
 }
